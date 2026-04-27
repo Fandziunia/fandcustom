4cf53931091a8a493aa8b5203cbc5f99c9c5c582
Fandziunia <m.danilo@o2.pl>
2026-04-27 18:15:42 +0200

    why

---
+++ src/main/java/net/fandziunia/fandcustom/FandCustomClient.java
@@ -0,0 +1,10 @@
+package net.fandziunia.fandcustom;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FandCustomClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
