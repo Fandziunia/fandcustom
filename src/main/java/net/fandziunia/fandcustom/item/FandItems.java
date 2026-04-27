7ab9dd2be4e88a2c858d0fa1d0cdaa07b005c8e9
Fandziunia <m.danilo@o2.pl>
2026-04-27 19:11:36 +0200

    Initializer fix - changed mappings to yarn. Fixed ExampleMixin injection.

--- src/main/java/net/fandziunia/fandcustom/item/FandItems.java
+++ src/main/java/net/fandziunia/fandcustom/item/FandItems.java
@@ -1,14 +1,15 @@
 package net.fandziunia.fandcustom.item;
 
 import net.fandziunia.fandcustom.FandCustom;
-import net.minecraft.core.Registry;
-import net.minecraft.core.registries.Registries;
-import net.minecraft.world.item.Item;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class FandItems {
 
     private static Item registerItem(String name, Item item){
-        return Registry.register(Registries.ITEM, new Identifier(FandCustom.MOD_ID, name), item);
+        return Registry.register(Registries.ITEM, Identifier.of(FandCustom.MOD_ID, name), item);
     }
 
     public static void registerModItems(){
